acfff3abdb77c07c37dfd48ab12628fcb45d0d76
xuzhendong <wb-xzd797743@alibaba-inc.com>
2024-06-28 15:33:36 +0800

    feat: 整理

--- src/views/ruleEngine/linkage/ActuatorDrawer1/Multiple.vue
+++ src/views/ruleEngine/linkage/ActuatorDrawer1/Multiple.vue
@@ -1,14 +1,11 @@
 <template>
   <el-tabs v-model="tabName" type="card" editable @edit="editRuleExecutorDtos">
-    <el-tab-pane
-      v-for="(item, i) in ruleExecutorDtos"
-      :label="`条件${i + 1}`"
-      :name="i"
-      :key="i"
-    >
+    <el-tab-pane v-for="(item, i) in ruleExecutorDtos" :label="`条件${i + 1}`" :name="i" :key="i">
+      <!-- 条件 -->
       <ActuatorItem v-model="item.executorConditions" />
-      <!-- <OpenAction />
-      <el-button class="add-relation_button" @click="addOffItem">
+      <!-- 条件为 true 的执行 -->
+      <OpenAction v-model="item.executorConfigs.branchActionTrue" />
+      <!-- <el-button class="add-relation_button" @click="addOffItem">
         + 添加执行关系duo
       </el-button>
       <el-form-item label="否则">
@@ -25,126 +22,35 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
 import ActuatorItem from './components/ActuatorItem.vue'
+import OpenAction from './components/OpenAction.vue'
 
 const tabName = ref(0)
 
 const ruleExecutorDtos = reactive([
-  {// 条件一
-    "executorConditions": [ // 条件表达式
+  {
+    executorConditions: [ // 条件表达式
       {
-        "condition_expression": [
+        condition_expression: [
           {
-            "expression_param": "pre", // 参数
-            "expression_operator": "<", // 操作   有（1.> 2.< 3.= 三种）
-            "expression_value": "1", // 参数值
+            expression_param: '', // 参数
+            expression_operator: '', // 操作   有（1.> 2.< 3.= 三种）
+            expression_value: '', // 参数值
           },
-          {
-            "expression_param": "hum",
-            "expression_operator": "=",
-            "expression_value": "60",
-            "expression_relation": "or"
-          },
-          {
-            "expression_param": "tem",
-            "expression_operator": ">",
-            "expression_value": "40",
-            "expression_relation": "and" // and:并且  or:或者  （当有前上一个元素时，该元素的condition_relation属性 才需要配置,第一个元素不需要配置）
-          }
         ]
       },
-      {
-        "condition_relation": "and", // and:并且  or:或者  （当有前上一个元素时，该元素的condition_relation属性 才需要配置，第一个元素不需要配置）
-        "condition_expression": [
-          {
-            "expression_param": "pre",
-            "expression_operator": "<",
-            "expression_value": "2",
-          },
-          {
-            "expression_param": "hum",
-            "expression_operator": "=",
-            "expression_value": "40",
-            "expression_relation": "and"
-          }
-        ]
-      }
     ],
-    "executorConfigs": {
-      "branchActionTrue": [ // 当条件执行为true时执行的分支
+    executorConfigs: {
+      branchActionTrue: [ // 当条件执行为true时执行的分支
         {
-          "type": "1", // 1.串行 2.并行
-          "actions": [
-            {
-              "executor_content": "deviceOutputNode", // 设备输入
-              "executor_config": {
-                "operator_type": "1", // 1.功能调用  2.属性读取  3.属性设置 选1就好
-                "executor_target": [ // 设备的id集合.如果用户选择了产品，没有选择设备，则自动选择产品下的所有设备
-                  "1791650558225674241"
-                ],
-                "function_id": "1792371474641072130", // 方法的Id
-                "params": { // 调用方法时的入参  /rest/auth/thingsModel/function/no-page/query 的  集合中每个元素的inputData字段中的集合
-                  "setTemperature": "55" // 请求参数  key：参数的唯一标识   value:用户输入的值
-                }
-              }
-            }
-          ]
+          type: '', // 1.串行 2.并行
+          actions: []
         },
-        {
-          "type": "2",
-          "actions": [
-            {
-              "executor_content": "deviceOutputNode", // 设备输入
-              "executor_config": {
-                "operator_type": "1", // 1.功能调用  2.属性读取  3.属性设置 选1就好
-                "executor_target": [ // 设备的id集合.如果用户选择了产品，没有选择设备，则自动选择产品下的所有设备
-                  "1791650558225674241"
-                ],
-                "function_id": "1792371474641072130", // 方法的Id
-                "params": { // 调用方法时的入参  /rest/auth/thingsModel/function/no-page/query 的  集合中每个元素的inputData字段中的集合
-                  "setTemperature": "55" // 请求参数  key：参数的唯一标识   value:用户输入的值
-                }
-              }
-            }
-          ]
-        }
       ],
-      "branchActionFalse": [ // 当条件执行为false时执行的分支
+      branchActionFalse: [ // 当条件执行为false时执行的分支
         {
-          "type": "1", // 1.串行 2.并行
-          "actions": [
-            {
-              "executor_content": "deviceOutputNode", // 设备输入
-              "executor_config": {
-                "operator_type": "1", // 1.功能调用  2.属性读取  3.属性设置 选1就好
-                "executor_target": [ // 设备的id集合.如果用户选择了产品，没有选择设备，则自动选择产品下的所有设备
-                  "1791650558225674241"
-                ],
-                "function_id": "1792371474641072130", // 方法的Id
-                "params": { // 调用方法时的入参  /rest/auth/thingsModel/function/no-page/query 的  集合中每个元素的inputData字段中的集合
-                  "setTemperature": "55" // 请求参数  key：参数的唯一标识   value:用户输入的值
-                }
-              }
-            }
-          ]
+          type: '', // 1.串行 2.并行
+          actions: []
         },
-        {
-          "type": "2",
-          "actions": [
-            {
-              "executor_content": "deviceOutputNode", // 设备输入
-              "executor_config": {
-                "operator_type": "1", // 1.功能调用  2.属性读取  3.属性设置 选1就好
-                "executor_target": [ // 设备的id集合.如果用户选择了产品，没有选择设备，则自动选择产品下的所有设备
-                  "1791650558225674241"
-                ],
-                "function_id": "1792371474641072130", // 方法的Id
-                "params": { // 调用方法时的入参  /rest/auth/thingsModel/function/no-page/query 的  集合中每个元素的inputData字段中的集合
-                  "setTemperature": "55" // 请求参数  key：参数的唯一标识   value:用户输入的值
-                }
-              }
-            }
-          ]
-        }
       ]
     }
   }
@@ -154,61 +60,33 @@ const editRuleExecutorDtos = (idx: number, action: 'remove' | 'add') => {
   if (action === 'remove') {
     ruleExecutorDtos.splice(idx, 1)
   } else {
-    ruleExecutorDtos.push({// 条件一
-    "executorConditions": [ // 条件表达式
-      {
-        "condition_expression": [
-          {
-            "expression_param": "", // 参数
-            "expression_operator": "", // 操作   有（1.> 2.< 3.= 三种）
-            "expression_value": "", // 参数值
-          },
-        ]
-      },
-    ],
-    "executorConfigs": {
-      "branchActionTrue": [ // 当条件执行为true时执行的分支
+    ruleExecutorDtos.push({
+      executorConditions: [ // 条件表达式
         {
-          "type": "1", // 1.串行 2.并行
-          "actions": [
+          condition_expression: [
             {
-              "executor_content": "deviceOutputNode", // 设备输入
-              "executor_config": {
-                "operator_type": "1", // 1.功能调用  2.属性读取  3.属性设置 选1就好
-                "executor_target": [ // 设备的id集合.如果用户选择了产品，没有选择设备，则自动选择产品下的所有设备
-                  "1791650558225674241"
-                ],
-                "function_id": "1792371474641072130", // 方法的Id
-                "params": { // 调用方法时的入参  /rest/auth/thingsModel/function/no-page/query 的  集合中每个元素的inputData字段中的集合
-                  "setTemperature": "55" // 请求参数  key：参数的唯一标识   value:用户输入的值
-                }
-              }
-            }
+              expression_param: '', // 参数
+              expression_operator: '', // 操作   有（1.> 2.< 3.= 三种）
+              expression_value: '', // 参数值
+            },
           ]
         },
       ],
-      "branchActionFalse": [ // 当条件执行为false时执行的分支
-        {
-          "type": "1", // 1.串行 2.并行
-          "actions": [
-            {
-              "executor_content": "deviceOutputNode", // 设备输入
-              "executor_config": {
-                "operator_type": "1", // 1.功能调用  2.属性读取  3.属性设置 选1就好
-                "executor_target": [ // 设备的id集合.如果用户选择了产品，没有选择设备，则自动选择产品下的所有设备
-                  "1791650558225674241"
-                ],
-                "function_id": "1792371474641072130", // 方法的Id
-                "params": { // 调用方法时的入参  /rest/auth/thingsModel/function/no-page/query 的  集合中每个元素的inputData字段中的集合
-                  "setTemperature": "55" // 请求参数  key：参数的唯一标识   value:用户输入的值
-                }
-              }
-            }
-          ]
-        },
-      ]
-    }
-  })
+      executorConfigs: {
+        branchActionTrue: [ // 当条件执行为true时执行的分支
+          {
+            type: '', // 1.串行 2.并行
+            actions: []
+          },
+        ],
+        branchActionFalse: [ // 当条件执行为false时执行的分支
+          {
+            type: '', // 1.串行 2.并行
+            actions: []
+          },
+        ]
+      }
+    })
   }
 }
 
--- src/views/ruleEngine/linkage/ActuatorDrawer1/components/OpenAction.vue
+++ src/views/ruleEngine/linkage/ActuatorDrawer1/components/OpenAction.vue
@@ -1,39 +1,104 @@
 <template>
-  <div>
-    <el-form-item label="执行" prop="conditions">
-      <el-select v-model="relationValue" placeholder="选择执行关系">
-        <el-option v-for="item in relation" :key="item.value" :label="item.label" :value="item.value" />
-      </el-select>
+  <div v-for="(item, idx) in branchActionTrue" :key="idx">
+    <el-form-item label="执行关系">
+      <div style="display: flex;width: 100%;">
+        <el-select v-model="item.type" placeholder="选择执行关系">
+          <el-option v-for="item in relation" :key="item.value" :label="item.label" :value="item.value" />
+        </el-select>
+        <el-icon
+          v-if="branchActionTrue?.length"
+          @click="delBranchActionTrue(idx)"
+          class="del-item"
+        >
+          <Delete />
+        </el-icon>
+      </div>
     </el-form-item>
-    <div v-if="relationValue === '1'" class="parallel">
-      <div class="parallel-list">读取 设备名称 参数名称</div>
+
+    <div class="parallel">
+      <div v-for="(action, idx) in item.actions" class="parallel-list" :key="idx">
+        {{ getActionsLabels(action) }}
+
+        <el-icon @click="delAction(item.actions, idx)" class="del-item">
+          <Delete />
+        </el-icon>
+      </div>
+
+      <el-button @click="openAction(item.actions)" class="add-action">
+        + 添加动作
+      </el-button>
     </div>
-    <div style="margin-bottom: 10px;">
-      <el-button class="inputAdd" @click="openAction">+ 添加动作</el-button>
-    </div>
-    <ActionModal ref="actionModalRef"/>
   </div>
+
+  <el-button
+    @click="addBranchActionTrue"
+    class="add-relation-button"
+  >
+    + 添加执行关系
+  </el-button>
+
+  <ActionModal ref="actionModalRef" />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, defineModel } from 'vue';
 import ActionModal from './ActionModal.vue';
-import { ruleEngine } from '@/api/interface';
+
+const branchActionTrue = defineModel<IBranchActionTrue>()
+
+const delAction = (actions: Record<string, any>[], idx: number) => {
+  actions.splice(idx, 1)
+}
+
+const addBranchActionTrue = () => {
+  branchActionTrue.value?.push({
+    type: '',
+    actions: [],
+  })
+}
+
+const delBranchActionTrue = (idx) => {
+  branchActionTrue.value?.splice(idx, 1)
+}
 
 const actionModalRef = ref<InstanceType<typeof ActionModal>>()
+const openAction = (actions) => {
+  function cb(action) {
+    actions.push(action)
+  }
 
-const relationValue = ref('');
+  actionModalRef.value?.open(cb)
+}
+
+type IBranchActionTrue = {
+  type: string
+  actions: object[]
+}[]
+
 const relation = [
   { value: '1', label: '并行' },
   { value: '2', label: '串行' }
 ]
 
-const openAction = (title: "编辑", row?: ruleEngine.linkage) => {
-  function cb(action) {
-    // offItem.actions.push(action)
-  }
+const getActionsLabels = (action: Record<string, any>) => {
+  const arr: string[] = []
 
-  actionModalRef.value?.open(cb)
+  if (action.executor_content) {
+    arr.push(`执行内容 ${action.executor_content.label};`)
+  }
+  if (action.product) {
+    arr.push(`执行对象 ${action.product.label}`)
+  }
+  if (action.executor_target?.length) {
+    arr.push(`执行设备 ${action.executor_target?.map(v => v.label).join('、')};`)
+  }
+  if (action.function_id) {
+    arr.push(`执行动作 ${action.function_id.label};`)
+  }
+  if (action.inputData?.length) {
+    arr.push(`温度设置 ${action.inputData[0].value}`)
+  }
+  return arr.join(' ')
 }
 </script>
 
@@ -49,10 +114,22 @@ const openAction = (title: "编辑", row?: ruleEngine.linkage) => {
   font-size: 14px;
   border: 1px solid #dcdfe6;
   border-radius: 3px;
+  display: flex;
 }
 
 .inputAdd {
   margin: 5px 60px;
   width: 84%
+}
+
+.add-action {
+  width: 100%;
+}
+.del-item {
+  cursor: pointer;
+  color: red;
+}
+.add-relation-button {
+  width: 100%;
 }
 </style>
